45b014ead557f7acc28f8bb30e0bf582d2aabac6
Lesley C <48189833+zen-out@users.noreply.github.com>
2022-04-01 08:06:54 +0800

    Update SkillsService.js

--- backend/services/SkillsService.js
+++ backend/services/SkillsService.js
@@ -48,8 +48,8 @@ module.exports = CharacterSkills
 let skillz = new CharacterSkills(knex)
 
 async function how_do_we_motivate_test() {
-    let audrey_skill_one = await skillz.createCharacterSkills(1, ["self_control", "nurture", "impromptu", "diplomacy", "cooking", "simplicity", "openmindedness", "host", "teach", "reassure", "support", "mentor"]);
-    let audrey_skill_two = await skillz.createCharacterSkills(1, ["tests", "readme", "", "git commit messages (huskey)", "publishing", "simplicity", "happy husband happy life", "friends on saturday", "teach", "talk to sam", "think about what cyrus needs", "get more massages"]);
+    let audrey_skill_one = await skillz.createCharacterSkills(1, ["self_control", "nurture", "conservative", "diplomacy", "cooking", "simplicity", "openmindedness", "host", "teach", "reassure", "support", "mentor"]);
+    let audrey_skill_two = await skillz.createCharacterSkills(1, ["tests", "readme", "trust in your own experiences", "git commit messages (huskey)", "publishing", "simplicity", "happy husband happy life", "friends on saturday", "teach", "stop having guy friends", "think about what cyrus needs", "get more massages"]);
 }
 
 how_do_we_motivate_test()
